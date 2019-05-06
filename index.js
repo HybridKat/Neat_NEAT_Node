87db1cd3e9f23d8c4364eb2bf9b5f75c3f4864e2
James McGrath <40497224+HybridKat@users.noreply.github.com>
2019-05-06 17:02:23 -0400

    Started brain object

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 var Jetty = require("jetty");
-const fixJetty = require("./src/configs/jettyPatch.js");
-global.jetty = fixJetty(new Jetty(process.stdout));
+  const fixJetty = require("./src/configs/jettyPatch.js");
+  global.jetty = fixJetty(new Jetty(process.stdout));
 
-jetty.nuke();
-jetty.moveTo([0,0]);
+const BRAIN = require("./src/brain");
 
-jetty.underline().rgb(10, false);
-  jetty.text("COL1");
-  jetty.column(58);
-  jetty.text("COL2");
-  jetty.column(72);
-  jetty.text("COL3");
-  jetty.column(91);
-  jetty.text("COL4");
-jetty.reset().rgb(11, false);
+var x = new BRAIN();
 
-jetty.lineDown().text("Testing lolz");
+// jetty.nuke();
+// jetty.moveTo([0,0]);
+//
+// jetty.underline().rgb(10, false);
+//   jetty.text("COL1");
+//   jetty.column(58);
+//   jetty.text("COL2");
+//   jetty.column(72);
+//   jetty.text("COL3");
+//   jetty.column(91);
+//   jetty.text("COL4");
+// jetty.reset().rgb(11, false);
+//
+// jetty.lineDown().text("Testing lolz");
